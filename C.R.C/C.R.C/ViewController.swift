4828fb38a58b0e636812062ecb13c885bcde0b38
jo030225 <whwngur123@naver.com>
2021-03-19 08:56:34 +0900

    [ADD] Alamofire 라이브러리 추가

--- C.R.C/C.R.C/ViewController.swift
+++ C.R.C/C.R.C/ViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sumPerson: UILabel!
+    @IBOutlet weak var currentPerson: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
