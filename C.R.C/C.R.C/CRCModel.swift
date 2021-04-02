660446f9d7fc4007cd47fc00b6e6853c74a2b0b7
jo030225 <whwngur123@naver.com>
2021-04-02 09:57:29 +0900

    [ADD] 백엔드 연결 및 현 인원과 남은 인원 수 구현

---
+++ C.R.C/C.R.C/CRCModel.swift
@@ -0,0 +1,12 @@
+//
+//  C.R.C.Model.swift
+//  C.R.C
+//
+//  Created by 조주혁 on 2021/04/02.
+//
+
+import Foundation
+
+struct CRCModel: Codable {
+    let student: Int
+}
--- C.R.C/C.R.C/ViewController.swift
+++ C.R.C/C.R.C/ViewController.swift
@@ -12,12 +12,32 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var sumPerson: UILabel!
     @IBOutlet weak var currentPerson: UILabel!
+    @IBOutlet weak var leftPerson: UILabel!
+    
+    var model: CRCModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        apiCall()
     }
-
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    func apiCall() {
+        let URL = "http://10.120.75.224:3000/"
+        AF.request(URL, method: .get).responseData { (data) in
+            guard let data = data.data else { return }
+            self.model = try? JSONDecoder().decode(CRCModel.self, from: data)
+            print(data)
+            self.currentPerson.text = "\(self.model?.student ?? 0)명"
+            self.leftPerson.text = "\(230 - (self.model?.student ?? 0))명"
+        }
+    }
+    
+    @IBAction func refreshButton(_ sender: Any) {
+        apiCall()
+    }
 }
 
